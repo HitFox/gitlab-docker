c96e3a04a0fc99559a7961b1a3542889312f948f
Martin Held <martin.held@hitfoxgroup.com>
2016-06-22 12:19:26 +0200

    fix env cron to work with GITLAB_OMNIBUS_CONFIG

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # export env vars to be used by cron
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
+printenv | egrep -v '^[a-z]|GITLAB_OMNIBUS_CONFIG|LS_COLORS|LESSOPEN|LESSCLOSE' |sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
+printf "$GITLAB_OMNIBUS_CONFIG" > GITLAB_OMNIBUS_CONFIG
+printf "export GITLAB_OMNIBUS_CONFIG=\"" >> /root/project_env.sh
+cat GITLAB_OMNIBUS_CONFIG >> /root/project_env.sh
+printf "\"" >> /root/project_env.sh
+
 # start cron
 cron
 # start gitlab
