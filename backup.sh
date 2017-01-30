27f098c588b8796c8beaadb56b61e28346004bd9
Juliano Vidal <juliano.vidal@elinvar.de>
2017-01-30 10:17:40 +0100

    Update remove old files command on backup script

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ tar -cvf $CONFIG_TAR /etc/gitlab/
 aws s3 cp $CONFIG_TAR s3://git.finleap.com
 
 # delete config tars older than 7 days
-find ./*.tar -mtime +7 -type f -delete
+find / -name *gitlab_backup.tar -mtime +7 -type f -delete
 
 # say hello to dead man snitch
 curl https://nosnch.in/e8ada076e2
